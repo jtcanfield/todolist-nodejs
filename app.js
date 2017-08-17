a5769377c56fe259c3962489a35854a325317f77
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-17 12:48:09 -0400

    add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function(req, res){
+  res.render("index");
+});
+
+app.listen(3000, function () {
+  console.log('Hosted on local:3000')
+})
