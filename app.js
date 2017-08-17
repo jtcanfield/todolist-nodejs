c563700697d5fe11e7e9987dd955b0260e43b862
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-17 15:57:30 -0400

    add js to find objects in array

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const file = './data.json';
 const fs = require('fs');
-const todosArray = ["Wash the car"];
+// const main = require("./public/main.js");
+// const todosArray = ["Wash the car"];
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 app.set('views', './views');
@@ -35,7 +36,7 @@ app.get("/", function (req, res) {
 //This means that every time method="post" is called on action="/", it will add to the array and redirect the user
 app.post("/", function (req, res) {
   var addtolist = req.body.inputtodo
-  todosArray.push(addtolist);
+  // todosArray.push(addtolist);
   fs.readFile('data.json', 'utf8', function readFileCallback(err, data){
       if (err){
           console.log(err);
@@ -56,7 +57,18 @@ app.post("/1", function (req, res) {
           console.log(err);
       } else {
       obj = JSON.parse(data); //now it an object
-      console.log(obj.todoArray);
+      // console.log(obj.todoArray);
+            // the code you're looking for
+            var needle = 'hi';
+            // iterate over each element in the array
+            for (var i = 0; i < obj.todoArray.length; i++){
+            // look for the entry with a matching `code` value
+              if (obj.todoArray[i] === "hi"){
+                console.log(obj.todoArray[i]);
+                 // we found it
+                // obj[i].name is the matched result
+              }
+            }
       // json = JSON.stringify(obj); //convert it back to json
       // fs.writeFile('data.json', json, 'utf8'); // write it back
   }});
